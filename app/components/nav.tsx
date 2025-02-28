9969fc6acaf3a5e2124b05bd5337d8d6d18533a6
Ian Lee <ianlee.tyl@gmail.com>
2025-02-28 16:10:47 -0500

    Removed two tabs

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -3,13 +3,7 @@ import Link from 'next/link'
 const navItems = {
   '/': {
     name: 'home',
-  },
-  '/blog': {
-    name: 'blog',
-  },
-  'https://vercel.com/templates/next.js/portfolio-starter-kit': {
-    name: 'deploy',
-  },
+  }
 }
 
 export function Navbar() {
